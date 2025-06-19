0cc018f7dcbb16a87592ade3e26cdc8231d1b7b8
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-19 19:57:15 +0300

    add function to login class

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -11,76 +11,71 @@ use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
-    // /**
-    //  * Determine if the user is authorized to make this request.
-    //  */
-    // public function authorize(): bool
-    // {
-    //     return true;
-    // }
 
-    // /**
-    //  * Get the validation rules that apply to the request.
-    //  *
-    //  * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-    //  */
-    // public function rules(): array
-    // {
-    //     return [
-    //         'email' => ['required', 'string', 'email'],
-    //         'password' => ['required', 'string'],
-    //     ];
-    // }
 
-    // /**
-    //  * Attempt to authenticate the request's credentials.
-    //  *
-    //  * @throws \Illuminate\Validation\ValidationException
-    //  */
-    // public function authenticate(): void
-    // {
-    //     $this->ensureIsNotRateLimited();
+    public function authorize() : bool
+    {
+        return true;
+    }
 
-    //     if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-    //         RateLimiter::hit($this->throttleKey());
 
-    //         throw ValidationException::withMessages([
-    //             'email' => trans('auth.failed'),
-    //         ]);
-    //     }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules() : array 
+    {
+        return [
+            'email' => ['required' , 'string' , 'email'],
+            'password' => ['required' , 'string'],
+        ];
 
-    //     RateLimiter::clear($this->throttleKey());
-    // }
+    }
 
-    // /**
-    //  * Ensure the login request is not rate limited.
-    //  *
-    //  * @throws \Illuminate\Validation\ValidationException
-    //  */
-    // public function ensureIsNotRateLimited(): void
-    // {
-    //     if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
-    //         return;
-    //     }
 
-    //     event(new Lockout($this));
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
 
-    //     $seconds = RateLimiter::availableIn($this->throttleKey());
+        event(new Lockout($this));
 
-    //     throw ValidationException::withMessages([
-    //         'email' => trans('auth.throttle', [
-    //             'seconds' => $seconds,
-    //             'minutes' => ceil($seconds / 60),
-    //         ]),
-    //     ]);
-    // }
+        $seconds = RateLimiter::availableIn($this->throttleKey());
 
-    // /**
-    //  * Get the rate limiting throttle key for the request.
-    //  */
-    // public function throttleKey(): string
-    // {
-    //     return Str::transliterate(Str::lower($this->string('email')).'|'.$this->ip());
-    // }
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
 
+
+
+    public function throttleKey() : string
+    {
+       return Str::transliterate(Str::lower($this->string('email')).'|'.$this->ip());
+    }
+    
+
+    public function authenticate() : void
+    {
+        $this->ensureIsNotRateLimited();
+        if(!Auth::attempt($this->only('email' , 'password'), $this->boolean('remember')))
+        {
+            Ratelimit::hit($this->throttleKey());
+           throw ValidationException::withMessages([
+            'email' => trans('auth.failed'),
+           ]);
+           Ratelimit::clear($this->throttleKey());
+        }
+
+    }
 }
