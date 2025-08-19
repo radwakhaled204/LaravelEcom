dc54ea91e901fc20941f9323cb434350aee97d35
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-19 20:49:55 +0300

    edit auth-admin

---
+++ routes/admin-auth.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\LoginController;
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('dashboard', function () {
+            // 👇 خلي الصفحة من برا فولدر admin
+            return view('dashboard');  
+        })->name('dashboard');
+    });
+});
+
