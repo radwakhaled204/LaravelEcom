8e4b90ffb86e28116e4f64800517dfebfe99224b
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-20 17:11:47 +0300

    fix

--- routes/admin-auth.php
+++ routes/admin-auth.php
@@ -10,7 +10,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::middleware('auth:admin')->group(function () {
         Route::get('dashboard', function () {
-            // 👇 خلي الصفحة من برا فولدر admin
+          
             return view('dashboard');  
         })->name('dashboard');
     });
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return redirect()->route('admin.login');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
